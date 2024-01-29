3b4115eabebaff0ba3a91951b035d479dd1b9dde
gabrielcristhie <gcristhie@hotmail.com>
2024-01-29 17:16:10 -0300

    primeiro commit

---
+++ Backend/blog/src/test/java/com/solides/blog/TangerinoBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solides.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TangerinoBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
