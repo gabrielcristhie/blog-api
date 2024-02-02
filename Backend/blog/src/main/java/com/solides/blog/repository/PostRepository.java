484f511ede80069403fb389cd870d893167d11f3
Gabriel Cristhiê Bandeira dos Santos <53788389+gabrielcristhie@users.noreply.github.com>
2024-02-01 22:12:08 -0300

    Criando repositories

---
+++ Backend/blog/src/main/java/com/solides/blog/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.solides.blog.repository;
+
+import com.solides.blog.domain.entities.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
